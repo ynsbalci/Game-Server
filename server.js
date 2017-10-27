c5f296527e582fa64b591d258003fd23b04f7681
ynsbalci <ynsbalci@outlook.com>
2017-10-27 23:28:54 +0300

    function oncesi

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+/**
+ * @Author: Yunus BALCI
+ * @Date:   2017-10-20T22:06:11+03:00
+ * @Email:  ynsbalci@outlook.com
+ * @Filename: server.js
+ * @Last modified by:   Yunus BALCI
+ * @Last modified time: 2017-10-27T23:27:09+03:00
+ */
+
+
+
 var io              = require('socket.io')(8000);
 var userId          = require('shortid');
 var roomId          = require('shortid');
 
-var usableRooms = [];
 var players = [];
+var usableRooms = [];
+var fullRooms = [];
+
 
 console.log(">> Server Is Runing");
 
@@ -11,59 +24,42 @@ io.sockets.on('connection', function(socket) {
 
     var currentPlayer = {
     	playerId: userId.generate(),
-    	playerNick:"playerNick",
+    	playerNick:"",
     	playerVehicleId:"",
     };
 
     var currentRoom = {
-    	roomId:"",
-		roomName:"",
-		roomPassword:"",
-		roomReady:0,
-		roomSpawned:0,
-		roomMaxSize:0,
-		roomLevelId:"",
-		roomUsers:[]
+      roomId:"",
+		  roomName:"",
+  		roomPassword:"",
+  		roomReady:0,
+  		roomSpawned:0,
+  		roomMaxSize:0,
+  		roomLevelId:"",
+  		roomUsers:[]
     };
 
 	players[currentPlayer.playerId] = currentPlayer;
 	players.push(currentPlayer);
 
-    console.log(">> " + currentPlayer.playerId + " Connected");
-    console.log(">> Players Length: " + players.length)
+  console.log(">> " + currentPlayer.playerId + " Connected");
+  console.log(">> Players Length: " + players.length)
 
-    socket.emit('CONNECT', currentPlayer)
+  socket.emit('CONNECT', currentPlayer)
 
 	socket.on('JOIN_OR_CREATE_ROOM', function (data){
+	   console.log(">> JOIN_OR_CREATE_ROOM");
+	   console.log("usableRooms: " + usableRooms.length);
 
-	console.log(">> JOIN_OR_CREATE_ROOM");
+	   var isJoined = false;
+	   if (usableRooms.length > 0) {
+		     JoinRoom(data);
+	   }
+     else {
+       console.log("bos oda yok");
+     }
 
-	console.log("usableRooms: " + usableRooms.length);
-
-	var isJoined = false;
-
-	if (usableRooms.length > 0) {
-
-		for (var i = 0; i < usableRooms.length; i++) {
-
-
-			if (usableRooms[i].roomUsers.length < parseInt(usableRooms[i].roomMaxSize) && usableRooms[i].roomName == data.roomName && usableRooms[i].roomPassword == data.roomPassword && usableRooms[i].roomReady < parseInt(usableRooms[i].roomMaxSize)) {
-					//
-
-				usableRooms[i].roomUsers.push(currentPlayer);
-				socket.join(usableRooms[i].roomId);
-
-				currentRoom = usableRooms[i];
-
-				socket.emit('JOIN_OR_CREATE_ROOM', currentRoom);
-				console.log("JOINED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomMaxSize + " " + currentRoom.roomUsers.length);
-
-				isJoined = true;
-				break;
-			}
-		}
-	}
-	if (!isJoined) {
+	/*if (!isJoined) {
 
 		currentRoom.roomId = roomId.generate();
 		currentRoom.roomName = data.roomName;
@@ -78,39 +74,17 @@ io.sockets.on('connection', function(socket) {
 
 		socket.emit('JOIN_OR_CREATE_ROOM', currentRoom);
 		console.log("CREATED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomUsers.length);
-	}
+	}*/
 
 	});
+
 
 
   socket.on('JOIN_ROOM', function (data){
     //
     if (usableRooms.length > 0) {
       //
-      for (var i = 0; i < usableRooms.length; i++) {
-
-        if (usableRooms[i].roomId != data.roomId) {
-          socket.emit('JOIN_ROOM_ERROR', "room not found!");
-        }
-        else if (usableRooms[i].roomName != data.roomName) {
-          socket.emit('JOIN_ROOM_ERROR', "room name wrong!")
-        }
-        else if (usableRooms[i].roomUsers.length > parseInt(usableRooms[i].roomMaxSize)) {
-          //hata var
-          socket.emit('JOIN_ROOM_ERROR', "room is full!")
-        }
-        else if (usableRooms[i].roomPassword != data.roomPassword) {
-          socket.emit('JOIN_ROOM_ERROR', "room password wrong!")
-        }
-        else// if (true) {
-          //herşey yolunda
-          usableRooms[i].roomUsers.push(currentPlayer);
-          socket.join(usableRooms[i].roomId);
-          currentRoom = usableRooms[i];
-          socket.emit('JOIN_ROOM', currentRoom);
-          console.log("JOINED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomMaxSize + " " + currentRoom.roomUsers.length);
-        }
-
+      JoinRoom(data);
   	}
     else {
       //
@@ -218,15 +192,14 @@ io.sockets.on('connection', function(socket) {
 		socket.broadcast.to(currentRoom.roomId).emit('USER_CONNECTED',currentPlayer);
 
 		//düzeltilcek
-		/*for (var i = 0; i < currentRoom.roomUsers.length; i++) {
+		for (var i = 0; i < currentRoom.roomUsers.length; i++) {
 			//
 			if (currentRoom.roomUsers[i] != currentPlayer) {
 				//
 				socket.emit('USER_CONNECTED', currentRoom.roomUsers[i])
 			}
 
-		}*/
-
+		}
 
 
 		currentRoom.roomSpawned++;
@@ -358,3 +331,31 @@ io.sockets.on('connection', function(socket) {
 	});
 
 });
+
+function JoinRoom(socket, data) {
+  console.log("JoinRoom");
+  for (var i = 0; i < usableRooms.length; i++) {
+
+    if (usableRooms[i].roomId != data.roomId) {
+      socket.emit('JOIN_ROOM_ERROR', "room not found!");
+    }
+    else if (usableRooms[i].roomName != data.roomName) {
+      socket.emit('JOIN_ROOM_ERROR', "room name wrong!")
+    }
+    else if (usableRooms[i].roomUsers.length > parseInt(usableRooms[i].roomMaxSize)) {
+      //hata var
+      socket.emit('JOIN_ROOM_ERROR', "room is full!")
+    }
+    else if (usableRooms[i].roomPassword != data.roomPassword) {
+      socket.emit('JOIN_ROOM_ERROR', "room password wrong!")
+    }
+    else{
+      //herşey yolunda
+      usableRooms[i].roomUsers.push(currentPlayer);
+      socket.join(usableRooms[i].roomId);
+      currentRoom = usableRooms[i];
+      socket.emit('JOIN_ROOM', currentRoom);
+      console.log("JOINED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomMaxSize + " " + currentRoom.roomUsers.length);
+    }
+  }
+}
