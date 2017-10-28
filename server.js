82ab1247cdcb02996106659d57f2ee4097308f88
ynsbalci <ynsbalci@outlook.com>
2017-10-28 19:16:22 +0300

    new server.js

--- server.js
+++ server.js
@@ -4,358 +4,322 @@
  * @Email:  ynsbalci@outlook.com
  * @Filename: server.js
  * @Last modified by:   Yunus BALCI
- * @Last modified time: 2017-10-27T23:27:09+03:00
+ * @Last modified time: 2017-10-28T19:15:10+03:00
  */
+ var io = require('socket.io')(8000);
+ var playerId = require('shortid');
+ var roomId = require('shortid');
+
+ var players=[];
+ var usableRooms=[];
+
+ console.log('\x1b[33m%s\x1b[0m',">> SERVER IS RUNNING");
 
 
+ io.sockets.on('connection', function(socket) {
 
-var io              = require('socket.io')(8000);
-var userId          = require('shortid');
-var roomId          = require('shortid');
+   var player = {
+     id:playerId.generate(),
+     nick:"",
+     vehicle_id:"",
+     spawn:false,
+   };
 
-var players = [];
-var usableRooms = [];
-var fullRooms = [];
+   var room = null;
 
+   players.push(player);
 
-console.log(">> Server Is Runing");
+   //
+   socket.emit('CONNECT', player);
 
-io.sockets.on('connection', function(socket) {
+   console.log(">> " + player.id + " CONNECTED");
 
-    var currentPlayer = {
-    	playerId: userId.generate(),
-    	playerNick:"",
-    	playerVehicleId:"",
-    };
+   //
+   socket.on('JOIN_ROOM', function (data){
+     //daha önce katılmışsa
+     if (room == null) {
+       //
+       room = JoinRoom(data);
+       //odaya gir
+       if (room) {
+         room.players.push(player);
+         socket.join(room.Id);
+         socket.emit('JOIN_ROOM', room);
+         console.log(">> JOINED " + room.id + " " + room.name + " " + room.pass + " " + room.max + " " + room.players.length);
 
-    var currentRoom = {
-      roomId:"",
-		  roomName:"",
-  		roomPassword:"",
-  		roomReady:0,
-  		roomSpawned:0,
-  		roomMaxSize:0,
-  		roomLevelId:"",
-  		roomUsers:[]
-    };
-
-	players[currentPlayer.playerId] = currentPlayer;
-	players.push(currentPlayer);
-
-  console.log(">> " + currentPlayer.playerId + " Connected");
-  console.log(">> Players Length: " + players.length)
-
-  socket.emit('CONNECT', currentPlayer)
-
-	socket.on('JOIN_OR_CREATE_ROOM', function (data){
-	   console.log(">> JOIN_OR_CREATE_ROOM");
-	   console.log("usableRooms: " + usableRooms.length);
-
-	   var isJoined = false;
-	   if (usableRooms.length > 0) {
-		     JoinRoom(data);
-	   }
+       }
+       else {
+         console.log(">> JOIN_ROOM_ERROR");
+         socket.emit('JOIN_ROOM_ERROR');
+       }
+     }
      else {
-       console.log("bos oda yok");
+       console.log(">> I HAVE ONE ROOM");
      }
 
-	/*if (!isJoined) {
+   });
 
-		currentRoom.roomId = roomId.generate();
-		currentRoom.roomName = data.roomName;
-		currentRoom.roomPassword = data.roomPassword;
-		currentRoom.roomMaxSize = data.roomMaxSize;
-		currentRoom.roomLevelId = data.roomLevelId;
-		currentRoom.roomUsers = [];
-		currentRoom.roomUsers.push(currentPlayer);
+   //
+   socket.on('JOIN_RANDOM', function (data){
+     //daha önce katılmamış sa
+     if (room == null) {
 
-		socket.join(currentRoom.roomId);
-		usableRooms.push(currentRoom);
+       room = JoinRandom(data);
+       //odaya gir
+       if (room) {
+         room.players.push(player);
+         socket.join(room.Id);
+         socket.emit('JOIN_ROOM', room);
+         console.log(">> JOINED " + room.id + " " + room.name + " " + room.pass + " " + room.type + " " + room.max + " " + room.players.length);
 
-		socket.emit('JOIN_OR_CREATE_ROOM', currentRoom);
-		console.log("CREATED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomUsers.length);
-	}*/
+       }
+       else {
+         console.log(">> JOIN_ROOM_ERROR");
+         socket.emit('JOIN_ROOM_ERROR');
+       }
+     }
+     else {
+       console.log(">> I HAVE ONE ROOM");
+     }
 
-	});
+   });
 
+   //
+   socket.on('CREATE_ROOM', function (data){
+     //
+     if (room == null) {
+       room = CreateRoom(data);
 
+       if (room != null) {
 
-  socket.on('JOIN_ROOM', function (data){
-    //
-    if (usableRooms.length > 0) {
+         socket.emit('CREATE_ROOM', room);
+         console.log(">> CREATED " + room.id + " " + room.name + " " + room.pass + " " + room.type + " " + room.max + " " + room.players.length);
+
+         //room a katılım
+         room.players.push(player);
+         socket.join(room.Id);
+         socket.emit('JOIN_ROOM', room);
+         console.log(">> JOINED " + room.id + " " + room.name + " " + room.pass + " " + room.type + " " + room.max + " " + room.players.length);
+
+       }else {
+         //
+         console.log(">> CREATE_ROOM_ERROR");
+         socket.emit('CREATE_ROOM_ERROR');
+       }
+     }
+     else {
+       console.log(">> I HAVE ONE ROOM");
+     }
+
+   });
+
+   //
+   socket.on('JOIN_OR_CREATE_ROOM', function (data){
+     //
+     //
+   });
+
+   //
+   socket.on('SPAWN', function (data){
+     //
+     console.log(">> SPAWNED " + player.id);
+
+     for (var i = 0; i < room.players.length; i++) {
+       if(room.players[i].spawn){
+         //
+         socket.emit('USER_CONNECTED', room.players[i]);
+       }
+     }
+     socket.emit('SPAWN', player);
+     player.spawn = true;
+     socket.broadcast.to(room.id).emit('USER_CONNECTED',player);
+
+     if (StartGame(room)) {
+       //
+       io.sockets.to(room.id).emit('START', room);
+     }
+
+   });
+
+   //
+   socket.on('MOVE', function (data){
+     console.log(">> MOVE: " + player.id + " " + room.id + " " + data.pos);
+     socket.broadcast.to(room.id).emit('MOVE', {
+       id: player.id,
+       pos: data.pos,
+       rot: data.rot
+     });
+   });
+
+   //
+   socket.on('SCORE', function (data){
+     console.log(">> SCORE");
+     io.sockets.to(room.id).emit('SCORE', {
+       id: currentPlayer.playerId,
+       order: data.order,
+       score: data.score
+     });
+   });
+
+  //
+  socket.on('EXIT', function (){
+		console.log(">>EXIT");
+    //oda ya katılmışsa
+    if (room) {
       //
-      JoinRoom(data);
-  	}
-    else {
+      console.log(">> " + player + " EXITED FROM " + room.id);
+      RemoveInRoom(room, player);
+
+      if (room.players.length == 0) {
+        //
+        DeleteRoom(room);
+      }
       //
-      socket.emit('JOIN_ROOM_ERROR', "no founs room");
+      socket.emit('EXIT');
+      socket.broadcast.to(room.id).emit('DISCONNECTED', {
+        id: player.id
+      });
+      room = null;
+
+
+    }else {
+      console.log(">> NO ROOM");
     }
-
   });
 
-
-  socket.on('CREATE_ROOM', function (data){
-    //
-    currentRoom.roomId = roomId.generate();
-		currentRoom.roomName = data.roomName;
-		currentRoom.roomPassword = data.roomPassword;
-		currentRoom.roomMaxSize = data.roomMaxSize;
-		currentRoom.roomLevelId = data.roomLevelId;
-		currentRoom.roomUsers = [];
-		currentRoom.roomUsers.push(currentPlayer);
-
-		socket.join(currentRoom.roomId);
-		usableRooms.push(currentRoom);
-
-		socket.emit('CREATE_ROOM', currentRoom);
-		console.log("CREATED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomUsers.length);
-
-  });
-
-
-  socket.on('JOIN_OR_CREATE_ROOM', function (data){
-
-	console.log(">> JOIN_OR_CREATE_ROOM");
-
-	console.log("usableRooms: " + usableRooms.length);
-
-	var isJoined = false;
-
-	for (var i = 0; i < usableRooms.length; i++) {
-
-		if (usableRooms.length > 0) {
-				//
-
-			if (usableRooms[i].roomUsers.length < parseInt(usableRooms[i].roomMaxSize) && usableRooms[i].roomName == data.roomName && usableRooms[i].roomPassword == data.roomPassword) {
-					//
-
-				usableRooms[i].roomUsers.push(currentPlayer);
-				socket.join(usableRooms[i].roomId);
-
-				currentRoom = usableRooms[i];
-
-				socket.emit('JOIN_OR_CREATE_ROOM', currentRoom);
-				console.log("JOINED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomMaxSize + " " + currentRoom.roomUsers.length);
-
-				isJoined = true;
-				break;
-			}
-		}
-	}
-	if (!isJoined) {
-
-		currentRoom.roomId = roomId.generate();
-		currentRoom.roomName = data.roomName;
-		currentRoom.roomPassword = data.roomPassword;
-		currentRoom.roomMaxSize = data.roomMaxSize;
-		currentRoom.roomLevelId = data.roomLevelId;
-		currentRoom.roomUsers = [];
-		currentRoom.roomUsers.push(currentPlayer);
-
-		socket.join(currentRoom.roomId);
-		usableRooms.push(currentRoom);
-
-		socket.emit('JOIN_OR_CREATE_ROOM', currentRoom);
-		console.log("CREATED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomUsers.length);
-	}
-
-	});
-
-
-	socket.on('READY', function (data){
-
-		currentRoom.roomReady++;
-		if (currentRoom.roomReady == parseInt(currentRoom.roomMaxSize)) {
-			//
-			io.sockets.to(currentRoom.roomId).emit('READY', currentRoom);
-			console.log(">> READY");
-
-			//allRooms.push(usableRooms.pop(usableRooms));
-		}
-
-		for (var i = 0; i < usableRooms.length; i++) {
-			console.log("READY: " + usableRooms[i].roomReady + " " + currentRoom.roomReady)
-		}
-
-
-	});
-
-    socket.on('SPAWN', function (data){
-
-		console.log(">> SPAWN");
-
-		currentPlayer.playerNick = data.playerNick;
-		currentPlayer.playerVehicleId = data.playerVehicleId;
-
-		socket.emit('SPAWN', currentPlayer)
-
-		socket.broadcast.to(currentRoom.roomId).emit('USER_CONNECTED',currentPlayer);
-
-		//düzeltilcek
-		for (var i = 0; i < currentRoom.roomUsers.length; i++) {
-			//
-			if (currentRoom.roomUsers[i] != currentPlayer) {
-				//
-				socket.emit('USER_CONNECTED', currentRoom.roomUsers[i])
-			}
-
-		}
-
-
-		currentRoom.roomSpawned++;
-
-		if (currentRoom.roomSpawned == parseInt(currentRoom.roomMaxSize)) {
-			//
-			io.sockets.to(currentRoom.roomId).emit('START', currentRoom);
-			console.log(currentRoom.roomId + " Game Started");
-		}
-
-
-	});
-
-    socket.on('MOVE', function (data){
-
-		socket.broadcast.to(currentRoom.roomId).emit('MOVE', {
-
-			playerId: currentPlayer.playerId,
-			playerPosition: data.playerPosition,
-			playerRotation: data.playerRotation
-		});
-
-	});
-
-	socket.on('SCORE', function (data){
-
-		io.sockets.to(currentRoom.roomId).emit('SCORE', {
-			playerId: currentPlayer.playerId,
-			playerOrder: data.playerOrder,
-			playerScore: data.playerScore
-		});
-
-	});
-
-	socket.on('PLAYER_LIST_IN_ROOMS', function (){
-
-		for (var roomUsers in currentRoom.roomUsers) {
-
-			socket.emit('PLAYER_LIST_IN_ROOMS', {
-				playerId: roomUsers.playerId,
-				playerNick: roomUsers.playerNick
-			});
-		}
-	});
-
-	socket.on('PLAYER_LIST_ALL', function (){
-
-		console.log(">> PLAYER_LIST_ALL");
-
-		for (var i = 0; i < players.length; i++) {
-			socket.emit('PLAYER_LIST_ALL', {
-				playerId: players[i].playerId,
-				playerNick: players[i].playerNick
-
-			});
-		}
-	});
-
-	socket.on('ROOM_LIST', function (){
+  //
+  socket.on('ROOM_LIST', function (){
 		//
 		console.log(">> ROOM_LIST: " + usableRooms.length);
-		for (var i = 0; i < usableRooms.length; i++) {
-			socket.emit('ROOM_LIST', usableRooms[i]);
-		}
-
-
-	});
-
-	socket.on('EXIT', function (){
-		//odadan sildim
-		console.log("EXIT");
-		for (var i = 0; i < usableRooms.length; i++) {
-			if (usableRooms[i] == currentRoom) {
-				//
-				for (var j = 0; j < usableRooms[i].roomUsers.length; j++) {
-					if (usableRooms[i].roomUsers[j] == currentPlayer) {
-						//
-						console.log("odadan sildim");
-						usableRooms[i].roomUsers.splice(j, 1);
-					}
-				}
-				if (usableRooms[i].roomUsers == 0) {
-					//
-					console.log("odayı sildim");
-					usableRooms.splice(i, 1)
-				}
-			}
-		}
-
-		socket.emit('EXIT');
+    for (var i = 0; i < usableRooms.length; i++) {
+      socket.emit('ROOM_LIST', usableRooms[i]);
+    }
 
 	});
 
-	socket.on('disconnect', function (){
+  //
+  socket.on('PLAYER_LIST', function (){
 
-		//players dan sildim
-		console.log(">> disconnect");
+		console.log(">> PLAYER_LIST: " + players.length);
 		for (var i = 0; i < players.length; i++) {
-			if (players[i].playerId == currentPlayer.playerId) {
-				//
-				players.splice(i,1);
-				console.log("player sildim");
-			}
+			socket.emit('PLAYER_LIST', {
+				id: players[i].id,
+				nick: players[i].nick
+			});
 		}
-
-		//odadan sildim
-		for (var i = 0; i < usableRooms.length; i++) {
-			if (usableRooms[i] == currentRoom) {
-				//
-				for (var j = 0; j < usableRooms[i].roomUsers.length; j++) {
-					if (usableRooms[i].roomUsers[j] == currentPlayer) {
-						//
-						console.log("odadan sildim");
-						usableRooms[i].roomUsers.splice(j, 1);
-					}
-				}
-				if (usableRooms[i].roomUsers == 0) {
-					//
-					console.log("odayı sildim");
-					usableRooms.splice(i, 1)
-				}
-			}
-		}
-
-		socket.broadcast.to(currentRoom.roomId).emit('DISCONNECTED', {
-			playerId: currentPlayer.playerId
-		});
-
 	});
 
-});
+  //
+  socket.on('PLAYERS_IN_ROOM', function (){
+    if (room) {
+      console.log(">> PLAYERS_IN_ROOM: " + room.players.length);
+      for (var i = 0; i < room.players.length; i++) {
+        socket.emit('PLAYERS_IN_ROOM', {
+          id: room.players[i].id,
+          nick: room.players[i].nick
+        });
+      }
+    }else {
+      console.log(">> NO ROOM");
+    }
+  });
 
-function JoinRoom(socket, data) {
-  console.log("JoinRoom");
-  for (var i = 0; i < usableRooms.length; i++) {
+  //
+  socket.on('disconnect', function (){
+    //
+    if (room) {
+      RemoveInRoom(room, player);
+      if (room.players.length == 0) {
+        //
+        DeleteRoom(room);
+      }
+    }
+    DeleteInPlayers(player);
 
-    if (usableRooms[i].roomId != data.roomId) {
-      socket.emit('JOIN_ROOM_ERROR', "room not found!");
-    }
-    else if (usableRooms[i].roomName != data.roomName) {
-      socket.emit('JOIN_ROOM_ERROR', "room name wrong!")
-    }
-    else if (usableRooms[i].roomUsers.length > parseInt(usableRooms[i].roomMaxSize)) {
-      //hata var
-      socket.emit('JOIN_ROOM_ERROR', "room is full!")
-    }
-    else if (usableRooms[i].roomPassword != data.roomPassword) {
-      socket.emit('JOIN_ROOM_ERROR', "room password wrong!")
-    }
-    else{
-      //herşey yolunda
-      usableRooms[i].roomUsers.push(currentPlayer);
-      socket.join(usableRooms[i].roomId);
-      currentRoom = usableRooms[i];
-      socket.emit('JOIN_ROOM', currentRoom);
-      console.log("JOINED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomMaxSize + " " + currentRoom.roomUsers.length);
-    }
-  }
-}
+  });
+
+
+ });
+
+ //FUNCTIONS
+ function JoinRoom(data) {
+   //
+   for (var i = 0; i < usableRooms.length; i++) {
+
+     if (usableRooms[i].id != data.id) {
+       return null;
+     }
+     else if (usableRooms[i].name != data.name) {
+       return null;
+     }
+     else if (usableRooms[i].players.length > parseInt(usableRooms[i].max)) {
+       //hata var
+       return null;
+     }
+     else if (usableRooms[i].pass != data.pass) {
+       return null;
+     }
+     else{
+       return  usableRooms[i];
+     }//else
+   }//for
+ }//function
+
+ function JoinRandom(data) {
+   //
+   for (var i = 0; i < usableRooms.length; i++) {
+
+     if (usableRooms[i].type == data.type) {
+       return usableRooms[i];
+     }
+
+   }//for
+ }//function
+
+ function CreateRoom(data) {
+   try {
+     var room = {
+       id:roomId.generate(),
+       name:data.name,
+       pass:data.pass,
+       ready:null,
+       max:data.max,
+       scene:data.scene,
+       type:data.type,
+       players:[],
+     };
+     usableRooms.push(room);
+     return room;
+   } catch (e) {
+     return null
+   }
+ }
+
+ function StartGame(room) {
+   //
+   for (var i = 0; i < room.players.length; i++) {
+     if (!room.players[i].spawn) {
+       return false;
+     }
+   }
+   return true;
+
+ }
+
+ function RemoveInRoom(room, player) {
+   console.log(">> " + player.id + " REMOVED FROM " + room.id);
+   room.players.splice(room.players.indexOf(player), 1);
+   console.log(">> PLAYERS.IN.ROOM: " + room.players.length);
+ }
+
+ function DeleteRoom(room) {
+   console.log(">> " + room.id + "DELETED");
+   usableRooms.splice(usableRooms.indexOf(room), 1);
+   console.log(">> ROOMS.LENGTH: " + usableRooms.length);
+ }
+
+ function DeleteInPlayers(player) {
+   console.log(">> " + player.id + " DISCONNECTED");
+   players.splice(players.indexOf(player), 1);
+   console.log(">> PLAYERS.LENGTH: " + players.length);
+ }
---
+++ server_eski.js
@@ -0,0 +1,364 @@
+/**
+ * @Author: Yunus BALCI
+ * @Date:   2017-10-20T22:06:11+03:00
+ * @Email:  ynsbalci@outlook.com
+ * @Filename: server.js
+ * @Last modified by:   Yunus BALCI
+ * @Last modified time: 2017-10-28T19:09:07+03:00
+ */
+
+
+
+var io              = require('socket.io')(8000);
+var userId          = require('shortid');
+var roomId          = require('shortid');
+
+var players = [];
+var usableRooms = [];
+var fullRooms = [];
+
+
+console.log(">> Server Is Runing");
+
+io.sockets.on('connection', function(socket) {
+
+    var currentPlayer = {
+    	playerId: userId.generate(),
+    	playerNick:"",
+    	playerVehicleId:"",
+    };
+
+    var currentRoom = {
+      roomId:"",
+		  roomName:"",
+  		roomPassword:"",
+  		roomReady:0,
+  		roomSpawned:0,
+  		roomMaxSize:0,
+  		roomLevelId:"",
+  		roomUsers:[]
+    };
+
+	players[currentPlayer.playerId] = currentPlayer;
+	players.push(currentPlayer);
+
+  console.log(">> " + currentPlayer.playerId + " Connected");
+  console.log(">> Players Length: " + players.length)
+
+  socket.emit('CONNECT', currentPlayer)
+
+	socket.on('JOIN_OR_CREATE_ROOM', function (data){
+	   console.log(">> JOIN_OR_CREATE_ROOM");
+	   console.log("usableRooms: " + usableRooms.length);
+
+	   var isJoined = false;
+	   if (usableRooms.length > 0) {
+		     JoinRoom(data);
+	   }
+     else {
+       console.log("bos oda yok");
+     }
+
+	/*if (!isJoined) {
+
+		currentRoom.roomId = roomId.generate();
+		currentRoom.roomName = data.roomName;
+		currentRoom.roomPassword = data.roomPassword;
+		currentRoom.roomMaxSize = data.roomMaxSize;
+		currentRoom.roomLevelId = data.roomLevelId;
+		currentRoom.roomUsers = [];
+		currentRoom.roomUsers.push(currentPlayer);
+
+		socket.join(currentRoom.roomId);
+		usableRooms.push(currentRoom);
+
+		socket.emit('JOIN_OR_CREATE_ROOM', currentRoom);
+		console.log("CREATED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomUsers.length);
+	}*/
+
+	});
+
+
+
+  socket.on('JOIN_ROOM', function (data){
+    //
+    if (usableRooms.length > 0) {
+      //
+      JoinRoom(data);
+  	}
+    else {
+      //
+      socket.emit('JOIN_ROOM_ERROR', "no founs room");
+    }
+
+  });
+
+
+  socket.on('CREATE_ROOM', function (data){
+    //
+    currentRoom.roomId = roomId.generate();
+		currentRoom.roomName = data.roomName;
+		currentRoom.roomPassword = data.roomPassword;
+		currentRoom.roomMaxSize = data.roomMaxSize;
+		currentRoom.roomLevelId = data.roomLevelId;
+		currentRoom.roomUsers = [];
+		currentRoom.roomUsers.push(currentPlayer);
+
+		socket.join(currentRoom.roomId);
+		usableRooms.push(currentRoom);
+
+		socket.emit('CREATE_ROOM', currentRoom);
+		console.log("CREATED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomUsers.length);
+
+  });
+
+
+  socket.on('JOIN_OR_CREATE_ROOM', function (data){
+
+	console.log(">> JOIN_OR_CREATE_ROOM");
+
+	console.log("usableRooms: " + usableRooms.length);
+
+	var isJoined = false;
+
+	for (var i = 0; i < usableRooms.length; i++) {
+
+		if (usableRooms.length > 0) {
+				//
+
+			if (usableRooms[i].roomUsers.length < parseInt(usableRooms[i].roomMaxSize) && usableRooms[i].roomName == data.roomName && usableRooms[i].roomPassword == data.roomPassword) {
+					//
+
+				usableRooms[i].roomUsers.push(currentPlayer);
+				socket.join(usableRooms[i].roomId);
+
+				currentRoom = usableRooms[i];
+
+				socket.emit('JOIN_OR_CREATE_ROOM', currentRoom);
+				console.log("JOINED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomMaxSize + " " + currentRoom.roomUsers.length);
+
+				isJoined = true;
+				break;
+			}
+		}
+	}
+	if (!isJoined) {
+
+		currentRoom.roomId = roomId.generate();
+		currentRoom.roomName = data.roomName;
+		currentRoom.roomPassword = data.roomPassword;
+		currentRoom.roomMaxSize = data.roomMaxSize;
+		currentRoom.roomLevelId = data.roomLevelId;
+		currentRoom.roomUsers = [];
+		currentRoom.roomUsers.push(currentPlayer);
+
+		socket.join(currentRoom.roomId);
+		usableRooms.push(currentRoom);
+
+		socket.emit('JOIN_OR_CREATE_ROOM', currentRoom);
+		console.log("CREATED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomUsers.length);
+	}
+
+	});
+
+
+	socket.on('READY', function (data){
+
+		currentRoom.roomReady++;
+		if (currentRoom.roomReady == parseInt(currentRoom.roomMaxSize)) {
+			//
+			io.sockets.to(currentRoom.roomId).emit('READY', currentRoom);
+			console.log(">> READY");
+
+			//allRooms.push(usableRooms.pop(usableRooms));
+		}
+
+		for (var i = 0; i < usableRooms.length; i++) {
+			console.log("READY: " + usableRooms[i].roomReady + " " + currentRoom.roomReady)
+		}
+
+
+	});
+
+    socket.on('SPAWN', function (data){
+
+		console.log(">> SPAWN");
+
+		currentPlayer.playerNick = data.playerNick;
+		currentPlayer.playerVehicleId = data.playerVehicleId;
+
+		socket.emit('SPAWN', currentPlayer)
+
+		socket.broadcast.to(currentRoom.roomId).emit('USER_CONNECTED',currentPlayer);
+
+		//düzeltilcek
+		for (var i = 0; i < currentRoom.roomUsers.length; i++) {
+			//
+			if (currentRoom.roomUsers[i] != currentPlayer) {
+				//
+				socket.emit('USER_CONNECTED', currentRoom.roomUsers[i])
+			}
+
+		}
+
+
+		currentRoom.roomSpawned++;
+
+		if (currentRoom.roomSpawned == parseInt(currentRoom.roomMaxSize)) {
+			//
+			io.sockets.to(currentRoom.roomId).emit('START', currentRoom);
+			console.log(currentRoom.roomId + " Game Started");
+		}
+
+
+	});
+
+    socket.on('MOVE', function (data){
+
+		socket.broadcast.to(currentRoom.roomId).emit('MOVE', {
+
+			playerId: currentPlayer.playerId,
+			playerPosition: data.playerPosition,
+			playerRotation: data.playerRotation
+		});
+
+	});
+
+	socket.on('SCORE', function (data){
+
+		io.sockets.to(currentRoom.roomId).emit('SCORE', {
+			playerId: currentPlayer.playerId,
+			playerOrder: data.playerOrder,
+			playerScore: data.playerScore
+		});
+
+	});
+
+	socket.on('PLAYER_LIST_IN_ROOMS', function (){
+
+		for (var roomUsers in currentRoom.roomUsers) {
+
+			socket.emit('PLAYER_LIST_IN_ROOMS', {
+				playerId: roomUsers.playerId,
+				playerNick: roomUsers.playerNick
+			});
+		}
+	});
+
+	socket.on('PLAYER_LIST_ALL', function (){
+
+		console.log(">> PLAYER_LIST_ALL");
+
+		for (var i = 0; i < players.length; i++) {
+			socket.emit('PLAYER_LIST_ALL', {
+				playerId: players[i].playerId,
+				playerNick: players[i].playerNick
+
+			});
+		}
+	});
+
+	socket.on('ROOM_LIST', function (){
+		//
+		console.log(">> ROOM_LIST: " + usableRooms.length);
+		for (var i = 0; i < usableRooms.length; i++) {
+			socket.emit('ROOM_LIST', usableRooms[i]);
+		}
+
+
+	});
+
+	socket.on('EXIT', function (){
+		//odadan sildim
+		console.log("EXIT");
+		for (var i = 0; i < usableRooms.length; i++) {
+			if (usableRooms[i] == currentRoom) {
+				//
+				for (var j = 0; j < usableRooms[i].roomUsers.length; j++) {
+					if (usableRooms[i].roomUsers[j] == currentPlayer) {
+						//
+						console.log("odadan sildim");
+						usableRooms[i].roomUsers.splice(j, 1);
+					}
+				}
+				if (usableRooms[i].roomUsers == 0) {
+					//
+					console.log("odayı sildim");
+					usableRooms.splice(i, 1)
+				}
+			}
+		}
+
+		socket.emit('EXIT');
+
+	});
+
+	socket.on('disconnect', function (){
+
+		//players dan sildim
+		console.log(">> disconnect");
+		for (var i = 0; i < players.length; i++) {
+			if (players[i].playerId == currentPlayer.playerId) {
+				//
+				players.splice(i,1);
+				console.log("player sildim");
+			}
+		}
+
+		//odadan sildim
+		for (var i = 0; i < usableRooms.length; i++) {
+			if (usableRooms[i] == currentRoom) {
+				//
+				for (var j = 0; j < usableRooms[i].roomUsers.length; j++) {
+					if (usableRooms[i].roomUsers[j] == currentPlayer) {
+						//
+						console.log("odadan sildim");
+						usableRooms[i].roomUsers.splice(j, 1);
+					}
+				}
+				if (usableRooms[i].roomUsers == 0) {
+					//
+					console.log("odayı sildim");
+					usableRooms.splice(i, 1)
+				}
+			}
+		}
+
+		socket.broadcast.to(currentRoom.roomId).emit('DISCONNECTED', {
+			playerId: currentPlayer.playerId
+		});
+
+	});
+
+});
+
+function JoinRoom(socket, data) {
+  console.log("JoinRoom");
+  for (var i = 0; i < usableRooms.length; i++) {
+
+    if (usableRooms[i].roomId != data.roomId) {
+      socket.emit('JOIN_ROOM_ERROR', "room not found!");
+    }
+    else if (usableRooms[i].roomName != data.roomName) {
+      socket.emit('JOIN_ROOM_ERROR', "room name wrong!")
+    }
+    else if (usableRooms[i].roomUsers.length > parseInt(usableRooms[i].roomMaxSize)) {
+      //hata var
+      socket.emit('JOIN_ROOM_ERROR', "room is full!")
+    }
+    else if (usableRooms[i].roomPassword != data.roomPassword) {
+      socket.emit('JOIN_ROOM_ERROR', "room password wrong!")
+    }
+    else{
+      //herşey yolunda
+      usableRooms[i].roomUsers.push(currentPlayer);
+      socket.join(usableRooms[i].roomId);
+      currentRoom = usableRooms[i];
+      socket.emit('JOIN_ROOM', currentRoom);
+      console.log("JOINED " + currentRoom.roomId + " " + currentRoom.roomName + " " + currentRoom.roomPassword + " " + currentRoom.roomMaxSize + " " + currentRoom.roomUsers.length);
+    }
+  }
+}
+
+//
+//console.log('\x1b[33m%s\x1b[0m', "stringToMakeYellow");  //yellow
